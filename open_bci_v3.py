28d6c234ccc0420203d8195ebb69558a97096656
Gabriel Ibagon <gabriel.ibagon@gmail.com>
2016-08-08 11:37:43 -0400

    fixed function call within print_register_settings

--- open_bci_v3.py
+++ open_bci_v3.py
@@ -348,7 +348,7 @@ class OpenBCIBoard(object):
   def print_register_settings(self):
     self.ser.write(b'?')
     time.sleep(0.5)
-    print_incoming_text();
+    self.print_incoming_text();
   #DEBBUGING: Prints individual incoming bytes
   def print_bytes_in(self):
     if not self.streaming:
