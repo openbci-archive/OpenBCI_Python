dc990b6e78ae81cbe325e6da1838afdf43f79f9b
AJ Keller <pushtheworldllc@gmail.com>
2018-10-17 09:29:07 -0400

    Update for 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'OpenBCI_Python',
-      version = '1.0.0',
+      version = '1.0.1',
       description = 'A lib for controlling OpenBCI Devices',
       author='AJ Keller',
       author_email='pushtheworldllc@gmail.com',
@@ -9,6 +9,6 @@ setup(name = 'OpenBCI_Python',
       packages=find_packages(),
       install_requires=['numpy'],
       url='https://github.com/openbci/openbci_python',  # use the URL to the github repo
-      download_url='https://github.com/openbci/openbci_python/archive/v1.0.0.tar.gz',
+      download_url='https://github.com/openbci/openbci_python/archive/v1.0.1.tar.gz',
       keywords=['device', 'control', 'eeg', 'emg', 'ekg', 'ads1299', 'openbci', 'ganglion', 'cyton', 'wifi'],  # arbitrary keywords
       zip_safe=False)
