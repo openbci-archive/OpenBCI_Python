99f80919cd0132628027a51c3666c31365d3e85b
suraj95 <srpatil@uci.edu>
2019-12-08 12:57:30 +0530

    Removed old commented out code

--- openbci/plugins/udp_server.py
+++ openbci/plugins/udp_server.py
@@ -18,36 +18,6 @@ import socket
 import plugin_interface as plugintypes
 
 
-# class PluginPrint(IPlugin):
-#   # args: passed by command line
-#   def activate(self, args):
-#     print("Print activated")
-#     # tell outside world that init went good
-#     return True
-
-#   def deactivate(self):
-#     print("Print Deactivated")
-
-#   def show_help(self):
-#     print("I do not need any parameter, just printing stuff.")
-
-#   # called with each new sample
-#   def __call__(self, sample):
-#     sample_string =
-#         "ID: %f\n%s\n%s" %(sample.id, str(sample.channel_data)[1:-1], str(sample.aux_data)[1:-1])
-#     print("---------------------------------")
-#     print(sample_string)
-#     print("---------------------------------")
-
-#     # DEBBUGING
-#     # try:
-#     #     sample_string.decode('ascii')
-#     # except UnicodeDecodeError:
-#     #     print("Not a ascii-encoded unicode string")
-#     # else:
-#     #     print(sample_string)
-
-
 class UDPServer(plugintypes.IPluginExtended):
     def __init__(self, ip='localhost', port=8888):
         self.ip = ip
@@ -63,7 +33,7 @@ class UDPServer(plugintypes.IPluginExtended):
         if len(self.args) > 1:
             self.port = int(self.args[1])
 
-        # init network
+        # initialize network
         print("Selecting raw UDP streaming. IP: " + self.ip + ", port: " + str(self.port))
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
